056e7d27bcd2a7b6b577450f05201874d018348e
Vlad Bespalyy <Jawa0131@gmail.com>
2024-08-21 17:10:07 +0300

    decided trucks

--- core-proto-vehicle/train-or-truck.js
+++ core-proto-vehicle/train-or-truck.js
@@ -4,26 +4,32 @@ function Vehicle(driver) {
   this.driver = driver;
   this.speed = 0;
 }
-function Train(driver) {}
-
-function Truck(driver) {
-  this.loadCargo = function (cargo) {};
+function Train(driver) {
+  Vehicle.call(this, driver);
 }
 
-Truck.prototype.unloadCargo = function () {
-  // Код тут.
-};
+function Truck(driver) {
+  Vehicle.call(this, driver);
+  this.cargo = [];
+  this.loadCargo = function (cargo) {
+    this.cargo.push(cargo);
+    return this.cargo;
+  };
+  this.unloadCargo = function () {
+    return this.cargo.pop(this.cargo[this.cargo.length]);
+  };
+}
+
 Vehicle.prototype.drive = function (kmh) {
-  return (this.speed = kmh);
+  return `${this.driver} driving at ${(this.speed = kmh)} kilometers per hour`;
 };
 Vehicle.prototype.stop = function () {
-  return (this.speed = 0);
+  this.speed = 0;
+  return `${this.driver} has stopped`;
 };
 Train.prototype = Object.create(Vehicle.prototype);
+Train.prototype.constructor = Train;
 Truck.prototype = Object.create(Vehicle.prototype);
+Truck.prototype.constructor = Truck;
 
-// экспорт Vehicle, Train, Truc в файл с тестами
 module.exports = { Vehicle, Train, Truck };
-
-// Функции-конструкторы Train и Truck имеют схожие свойства и поведение.
-// Перепишите их так, чтобы избавиться от дублирования одинаковых свойств и методов.
