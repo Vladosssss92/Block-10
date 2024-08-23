22eec7b87cd45260d316ef95f23ac349b68570d1
Vlad Bespalyy <Jawa0131@gmail.com>
2024-08-23 12:06:55 +0300

    decided cartoon

--- core-proto-disney/src/cartoon.js
+++ core-proto-disney/src/cartoon.js
@@ -9,7 +9,7 @@ function Cartoon(
   title,
   year,
   forChildren = true,
-  beginning = "Long, long ago..."
+  beginning = "Long, long ago"
 ) {
   this.title = title;
   this.year = year;
@@ -17,30 +17,44 @@ function Cartoon(
   this.beginning = beginning;
 }
 
-Cartoon.prototype.getDescription = function () {};
+Cartoon.prototype.getDescription = function () {}; // нахрена этот объект вообще?)))
 
-Cartoon.prototype.play = function () {};
+Cartoon.prototype.play = function () {
+  return [this.studio, this.studioLogo, this.title, this.beginning];
+};
 
 /**
  * Мульт студии Walt Disney.
  */
-function DisneyCartoon(title, year, forChildren, beginning) {
+function DisneyCartoon(
+  title,
+  year,
+  forChildren,
+  beginning = "Long, long ago in a faraway land..."
+) {
   Cartoon.call(this, title, year, forChildren, beginning);
   this.studio = "Walt Disney";
+  this.studioLogo = "🏰🌠";
 }
 
-DisneyCartoon.prototype = Object.create(Cartoon);
+DisneyCartoon.prototype = Object.create(Cartoon.prototype);
 DisneyCartoon.prototype.constructor = DisneyCartoon;
 
 /**
  * Мульт студии DreamWorks.
  */
-function DreamWorksCartoon(title, year, forChildren, beginning) {
+function DreamWorksCartoon(
+  title,
+  year,
+  forChildren,
+  beginning = "Long, long ago in a faraway land..."
+) {
   Cartoon.call(this, title, year, forChildren, beginning);
   this.studio = "DreamWorks";
+  this.studioLogo = "🌙";
 }
 
-DreamWorksCartoon.prototype = Object.create(Cartoon);
+DreamWorksCartoon.prototype = Object.create(Cartoon.prototype);
 DreamWorksCartoon.prototype.constructor = DreamWorksCartoon;
 
-// module.exports = { Cartoon, DisneyCartoon, DreamWorksCartoon };
+module.exports = { Cartoon, DisneyCartoon, DreamWorksCartoon };
