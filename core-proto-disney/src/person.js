df1cfcb88ce68f7a75901b02b1603a1886fb0718
Vlad Bespalyy <Jawa0131@gmail.com>
2024-08-24 17:49:32 +0300

    one test fail...

--- core-proto-disney/src/person.js
+++ core-proto-disney/src/person.js
@@ -2,12 +2,48 @@
  * Человек.
  * @param {string} name
  */
-function Person(name) {
-  // Код тут.
+function Person(name, [movie]) {
+  this.name = name;
+  this.watchedMovies = [];
+  this.watchedMovies.__proto__.toString = function () {
+    this.forEach((element, index) => {
+      return `${index + 1} ${element.title}`;
+    });
+  };
+  this.mult = [];
+  this.watchMovie(movie);
 }
 
 Person.prototype.watchMovie = function (movie) {
-  // Код тут.
+  // movie.play();
+  if (this.watchedMovies.length === 0) {
+    this.watchedMovies.push(movie);
+  } else if (!this.mult.includes(movie.title)) {
+    this.watchedMovies.push(movie);
+  }
+  this.mult.push(movie.title);
 };
 
-// module.exports = { Person };
+// let zzz = {
+//   beginning: "Long, long ago in a faraway land...",
+//   forChildren: true,
+//   studio: "Walt Disney",
+//   studioLogo: "🏰🌠",
+//   title: "Rapunzel",
+//   year: 0,
+// };
+let emm = new Person("emma", [{ title: "rap", sex: "34", xxx: "wer" }]);
+console.log(emm);
+console.log(emm.watchedMovies);
+console.log(emm.mult);
+emm.watchMovie({ title: "rrr" });
+console.log(emm.mult);
+emm.watchMovie({ title: "rrr", sss: 123 });
+console.log(emm.mult);
+console.log(emm.mult.includes("rap"));
+console.log(emm.watchedMovies);
+
+console.log(emm.watchedMovies.toString());
+console.log(emm.watchedMovies.__proto__ === Array.prototype);
+console.log(Array.prototype);
+module.exports = { Person };
--- core-proto-vehicle/spec/proto.spec.js
+++ core-proto-vehicle/spec/proto.spec.js
@@ -1,77 +1,77 @@
-// const { Vehicle, Train, Truck } = require("../train-or-truck");
+const { Vehicle, Train, Truck } = require("../train-or-truck");
 
-// describe("Vehicle prototype", () => {
-//   let vehicle;
-//   beforeEach(() => {
-//     vehicle = new Vehicle("test");
-//   });
-//   test("has a drive method", () => {
-//     expect(vehicle.drive).toEqual(expect.any(Function));
-//   });
-// });
+describe("Vehicle prototype", () => {
+  let vehicle;
+  beforeEach(() => {
+    vehicle = new Vehicle("test");
+  });
+  test("has a drive method", () => {
+    expect(vehicle.drive).toEqual(expect.any(Function));
+  });
+});
 
-// describe("Train", () => {
-//   describe("prototype constructor", () => {
-//     test("is Train", () => {
-//       expect(Train.prototype.constructor).toEqual(Train);
-//     });
-//   });
+describe("Train", () => {
+  describe("prototype constructor", () => {
+    test("is Train", () => {
+      expect(Train.prototype.constructor).toEqual(Train);
+    });
+  });
 
-//   describe(".drive()", () => {
-//     let train;
-//     let vehicle;
-//     beforeEach(() => {
-//       train = new Train("test");
-//       vehicle = new Vehicle("test");
-//     });
+  describe(".drive()", () => {
+    let train;
+    let vehicle;
+    beforeEach(() => {
+      train = new Train("test");
+      vehicle = new Vehicle("test");
+    });
 
-//     test("is the same function as Vehicle.prototype.drive", () => {
-//       expect(train.drive()).toBe(vehicle.drive());
-//     });
-//   });
-// });
+    test("is the same function as Vehicle.prototype.drive", () => {
+      expect(train.drive()).toBe(vehicle.drive());
+    });
+  });
+});
 
-// describe("Truck", () => {
-//   describe("prototype constructor", () => {
-//     test("is Truck", () => {
-//       expect(Truck.prototype.constructor).toEqual(Truck);
-//     });
-//   });
+describe("Truck", () => {
+  describe("prototype constructor", () => {
+    test("is Truck", () => {
+      expect(Truck.prototype.constructor).toEqual(Truck);
+    });
+  });
 
-//   describe(".drive()", () => {
-//     let truck;
-//     let vehicle;
-//     beforeEach(() => {
-//       truck = new Truck("test");
-//       vehicle = new Vehicle("test");
-//     });
+  describe(".drive()", () => {
+    let truck;
+    let vehicle;
+    beforeEach(() => {
+      truck = new Truck("test");
+      vehicle = new Vehicle("test");
+    });
 
-//     test("is the same function as Vehicle.prototype.drive", () => {
-//       expect(truck.drive()).toBe(vehicle.drive());
-//     });
-//   });
+    test("is the same function as Vehicle.prototype.drive", () => {
+      expect(truck.drive()).toBe(vehicle.drive());
+    });
+  });
 
-//   describe(".loadCargo()", () => {
-//     let truck;
-//     beforeEach(() => {
-//       truck = new Truck("test");
-//     });
+  describe(".loadCargo()", () => {
+    let truck;
+    beforeEach(() => {
+      truck = new Truck("test");
+    });
 
-//     test("is on the prototype", () => {
-//       expect(Truck.prototype.loadCargo).toEqual(expect.any(Function));
-//       expect(truck.loadCargo).toBe(Truck.prototype.loadCargo);
-//     });
-//   });
+    test("is on the prototype", () => {
+      expect(Truck.prototype.loadCargo).toEqual(expect.any(Function));
+      expect(truck.loadCargo).toBe(Truck.prototype.loadCargo);
+    });
+  });
 
-//   describe(".unloadCargo()", () => {
-//     let truck;
-//     beforeEach(() => {
-//       truck = new Truck("test");
-//     });
+  describe(".unloadCargo()", () => {
+    let truck;
+    beforeEach(() => {
+      truck = new Truck("test");
+    });
 
-//     test("is on the prototype", () => {
-//       expect(Truck.prototype.unloadCargo).toEqual(expect.any(Function));
-//       expect(truck.unloadCargo).toBe(Truck.prototype.unloadCargo);
-//     });
-//   });
-// });
+    test("is on the prototype", () => {
+      expect(Truck.prototype.unloadCargo).toEqual(expect.any(Function));
+      expect(truck.unloadCargo).toBe(Truck.prototype.unloadCargo);
+    });
+  });
+});
